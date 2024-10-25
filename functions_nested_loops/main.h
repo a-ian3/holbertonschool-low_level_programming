6ba0c89d58d5f5a6c51e02c9ab6d6c933d51dfda
root <arianeediatta@icloud.com>
2024-10-25 15:51:19 +0000

    the same task

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,4 +1,1 @@
-julien@ubuntu:~/$  gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 0-putchar.c -o 0-putchar
-julien@ubuntu:~/$ ./0-putchar 
-_putchar
-julien@ubuntu:~/$ 
+int_putchar(char c); 
