5d43e76fce272c79a45378ee0f8bf335fabf43ac
root <arianeediatta@icloud.com>
2024-10-25 15:42:44 +0000

    the same task

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,1 +1,4 @@
-hgytuyjgvyh,bvhjdutfu
+julien@ubuntu:~/$  gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 0-putchar.c -o 0-putchar
+julien@ubuntu:~/$ ./0-putchar 
+_putchar
+julien@ubuntu:~/$ 
